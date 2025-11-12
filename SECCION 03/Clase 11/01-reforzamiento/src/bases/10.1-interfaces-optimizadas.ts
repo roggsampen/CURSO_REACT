413e7eef16b34ec5930663e199755eb1a07bb7fd
roggsampen <roggsampen@gmail.com>
2025-11-12 01:16:02 -0500

    cambios

---
+++ SECCION 03/Clase 11/01-reforzamiento/src/bases/10.1-interfaces-optimizadas.ts
@@ -0,0 +1,26 @@
+import type { GiphyResponse, Gif } from "../data/giphy.response";
+
+// FETCH API
+const API_KEY = 'vb772cLJ4jE5u6CN93cURsaT3gKR1BCn';
+
+const myRequest = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`);
+
+//PROMESAS EN CADENA
+myRequest.then( (response) => response.json()
+.then( ({data: datae}: GiphyResponse) => {
+    //console.log(datae);
+    
+    //const imageUrl = datae.data.images.original.url;
+    const imageUrl = datae.images.original.url;
+    console.log(imageUrl);
+
+    //creamos un elemento <img> en el main y insertamos la imagen
+    const imgElement = document.createElement('img');
+    imgElement.src = imageUrl;
+
+    document.body.append(imgElement);
+    
+}))
+.catch((err) => console.error(err));
+
+
