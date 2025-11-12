7f29ec120ff13b912942c68153cf137a861254b4
roggsampen <roggsampen@gmail.com>
2025-11-12 01:22:00 -0500

    cambios

--- SECCION 03/Clase 11/01-reforzamiento/src/bases/10.1-interfaces-optimizadas.ts
+++ SECCION 03/Clase 11/01-reforzamiento/src/bases/10.1-interfaces-optimizadas.ts
@@ -5,21 +5,22 @@ const API_KEY = 'vb772cLJ4jE5u6CN93cURsaT3gKR1BCn';
 
 const myRequest = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`);
 
+//Funcion para crear un elemento <img> en el main y insertamos la imagen
+const createImageInsideDOM = (url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+
+    document.body.append(imgElement);
+}
+
 //PROMESAS EN CADENA
 myRequest.then( (response) => response.json()
 .then( ({data: datae}: GiphyResponse) => {
     //console.log(datae);
-    
     //const imageUrl = datae.data.images.original.url;
     const imageUrl = datae.images.original.url;
     console.log(imageUrl);
-
-    //creamos un elemento <img> en el main y insertamos la imagen
-    const imgElement = document.createElement('img');
-    imgElement.src = imageUrl;
-
-    document.body.append(imgElement);
-    
+    createImageInsideDOM(imageUrl);
 }))
 .catch((err) => console.error(err));
 
